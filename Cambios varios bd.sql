a7bbffaef3869117d6fcecd22c316406b9d01567
Kevin Irías <kevin.irias@inaxir.com>
2016-04-24 14:31:31 -0600

    Cambios visuales

--- Cambios varios bd.sql
+++ Cambios varios bd.sql
@@ -1,10 +1,43 @@
-﻿CREATE OR REPLACE FUNCTION add_eventos( descrip varchar(250), u varchar(250),idcat varchar(8), fcha date, c varchar(80), p varchar(80) ,lat decimal(7,3), longit decimal(7,3)) 
+﻿
+CREATE OR REPLACE FUNCTION add_eventos( descrip varchar(250), u varchar(250),idcat varchar(8), fcha date, c varchar(80), p varchar(80) ,lat decimal(7,3), longit decimal(7,3)) 
     RETURNS void AS $$
+	DECLARE id_e INTEGER;
     BEGIN
-      INSERT INTO eventos(descripcion, url, idcategoria,fecha, ciudad, pais, latitud, longitud) VALUES (descrip, u, fcha, c, p, lat, longit);
+      INSERT INTO eventos(descripcion, url, idcategoria, fecha, ciudad, pais, latitud, longitud) 
+      VALUES (descrip, u, idcat,fcha, c, p, lat, longit) RETURNING id_evento INTO id_e;
+      INSERT INTO localizacion values(lat, longit, id_e);
     END;
     $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION show_points() RETURNS refcursor AS $$
+    DECLARE
+      ref refcursor;
+    BEGIN
+      OPEN ref FOR SELECT localizacion.cordx, localizacion.cordy, eventos.descripcion, eventos.fecha, eventos.idcategoria, eventos.ciudad, eventos.pais, eventos.url 
+      FROM localizacion INNER JOIN eventos ON localizacion.id_evento=eventos.id_evento;
+      RETURN ref;
+    END;
+    $$ LANGUAGE plpgsql;
+
+
+SELECT show_points();
+
+
+
+BEGIN;
+ 
+   SELECT show_points();
+   -- Returns: <unnamed portal 2>
+ 
+   FETCH ALL IN "<unnamed portal 5>";
+COMMIT;CREATE TABLE localizacion(
+cordx decimal(7,3) NOT NULL,
+cordy decimal(7,3) NOT NULL,
+id_evento integer
+);
+
+
+    
 select*from eventos
 select*from categoria
 INSERT INTO categoria values('avc', 'AVIACION')
@@ -42,3 +75,14 @@ ADD CONSTRAINT fk_eventos
 FOREIGN KEY (id_evento) 
 REFERENCES eventos
 ON DELETE CASCADE;
+
+SELECT show_points();
+SELECT add_eventos('METEORITO NICARAGUA', 'NINGUNO', 'esp', '14/03/2015', 'MANAGUA', 'NICARAGUA', -86.3502, 12.0564);
+
+select*from eventos
+select*from localizacion
+truncate table eventos CASCADE
+drop table localizacion
+
+truncate table eventos
+
