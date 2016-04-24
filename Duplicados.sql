73160b90b5132f36ec833ec4cb69c61dfcba4e53
Kevin Irías <kevin.irias@inaxir.com>
2016-04-23 23:49:22 -0600

    Cambios en BD de ciudades
    Se eliminaron duplicados de la bd de ciudades.

---
+++ Duplicados.sql
@@ -0,0 +1,8 @@
+﻿create table GeoLiteCity (country character(2),
+    region character(2),city varchar(60),
+    latitude decimal(7,3),longitude decimal(7,3));
+
+ insert into GeoLiteCity (country,region,city,latitude,longitude) 
+   (select country,region,city,avg(latitude),avg(longitude) 
+    from gepiplitecity group by country,region,city);
+    delete from GeoLiteCity where country='HN' and city = 'Managua'
