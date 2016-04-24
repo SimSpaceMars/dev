9503dea4587b542817b39d832a67d94e8b310593
Kevin Irías <kevin.irias@inaxir.com>
2016-04-23 21:50:54 -0600

    Cambios en estructura de tablas
    Se realizo cambios en tabla eventos y se agrego una nueva tabla.

---
+++ cambios tablas.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE eventos
+ADD column fecha date not null,
+ADD column lugar varchar(100) NOT NULL
+
+
+CREATE TABLE solicitud_evt()INHERITS(eventos);
+
+ALTER TABLE solicitud_evt
+ADD COLUMN verificada boolean
