8db1619ae5c5109c0e67bc627e3ab14ba87ea5e6
Kevin Irías <kevin.irias@inaxir.com>
2016-04-23 22:10:09 -0600

    Se agregó ciudad y país como campos.

--- cambios tablas.sql
+++ cambios tablas.sql
@@ -1,9 +1,15 @@
 ﻿ALTER TABLE eventos
+ADD column latitud decimal(3,2) NOT NULL,
+ADD column longitud decimal(3,2) NOT NULL,
 ADD column fecha date not null,
-ADD column lugar varchar(100) NOT NULL
+ADD column ciudad varchar(80) NOT NULL,
+ADD column pais varchar(80) NOT NULL
 
 
 CREATE TABLE solicitud_evt()INHERITS(eventos);
 
 ALTER TABLE solicitud_evt
 ADD COLUMN verificada boolean
+
+select*from eventos
+select*from solicitud_evt
