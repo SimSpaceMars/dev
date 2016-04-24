81db3478e797f0bc3954e9633d615971ce4a9c2b
Kevin Irías <kevin.irias@inaxir.com>
2016-04-23 23:32:25 -0600

    Cambios significativos BD
    Se agregó una BD de ciudades del mundo con su latitud y longitud. El
    import del archivo .csv se tiene que hacer mediante el pgAdmin.

---
+++ BD Ciudades.sql
@@ -0,0 +1,4 @@
+﻿create table GepIPLiteCity (locId integer, country character(2), region text, city text,
+postalCode text, latitude decimal(7,3), longitude decimal(7,3), metroCode integer, areaCode integer);
+
+select*from gepiplitecity where city='Matagalpa';
